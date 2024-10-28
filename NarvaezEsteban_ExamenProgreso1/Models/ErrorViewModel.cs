d3c652fd2fb8ba3fd97c791d3e98639cce97004d
laboratorio <laboratorio@UPOAULA10222>
2024-10-28 07:16:10 -0500

    Agregar archivos de proyecto.

---
+++ NarvaezEsteban_ExamenProgreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NarvaezEsteban_ExamenProgreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
